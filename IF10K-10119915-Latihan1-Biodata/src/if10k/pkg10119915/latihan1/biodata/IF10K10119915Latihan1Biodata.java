d761e9b7c0358099e5dcaa0160e05de6391c272a
hongki09 <73209580+hongki09@users.noreply.github.com>
2020-10-21 10:20:52 +0700

    Add files via upload
    first commit

---
+++ IF10K-10119915-Latihan1-Biodata/src/if10k/pkg10119915/latihan1/biodata/IF10K10119915Latihan1Biodata.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119915.latihan1.biodata;
+
+/**
+ *
+ * @author 
+ * Nama      : M Hongki Alfikram
+ * NIM       : 10119915
+ * Kelas     : IF-10K
+ * Deskripsi : Perintah menampilkan value ke layar.
+ */
+public class IF10K10119915Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "----------------");
+        System.out.println("Nama\t\t: M Hongki Alfikram");
+        System.out.println("Alamat\t\t: Jl. Ibrahim Adjie no.255");
+        System.out.println("Tempat Lahir\t: Bandung");
+        System.out.println("Tanggal Lahir\t: 15 Januari 1999");
+        System.out.println("----------------");
+        System.out.println("Hobi\t\t: lari");
+        System.out.println("Makes\t\t: bakso");
+        System.out.println("Mikes\t\t: es teh");
+        
+    }
+    
+}
